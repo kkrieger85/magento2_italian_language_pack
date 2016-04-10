2e2116ac689625d64a87770f6a5b43e98c4de46b
Kevin Krieger <kk@kkrieger.de>
2016-04-10 13:39:26 +0200

    fix vendor

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'tsr_it_it',
+    'kkrieger85_it_it',
     __DIR__
 );
